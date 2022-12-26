3a15786f23598a9cb31e081d4322cd05065c1cd5
badrli <Windows199699!>
2022-12-26 22:50:59 +0100

    Finished Day1 of AoC

---
+++ Day1/Aufg2.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    counter = -1
+    resultArray = [0 for i in range(500)]
+    f = open("Input", "r")
+    inputString = f.read()
+    arrayOfAllNumbers = inputString.split("\n\n")
+
+    for array in arrayOfAllNumbers:
+        counter = counter + 1
+        resultArray[counter] = 0
+        numbersInArray = array.split("\n")
+        for number in numbersInArray:
+            if resultArray[counter] == 0:
+                resultArray[counter] = int(number)
+            else:
+                resultArray[counter] += int(number)
+
+    counter = 0
+    result = 0
+
+    while counter < 3:
+        result += max(resultArray)
+        resultArray.remove(max(resultArray))
+        counter += 1
+
+
+
+    print(result)
